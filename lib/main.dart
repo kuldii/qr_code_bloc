3c3847c6674cfbaa4515907b3be7393882c0564d
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2023-04-25 08:48:20 +0500

    downgrade project

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+void main() {
   runApp(const MyApp());
 }
 
